acf63ea43d68ed05b3f56f7dfe2fc5b40ae4c085
Saman <arifs@uwindsor.ca>
2017-07-06 17:26:33 -0400

    First Commit

---
+++ Assets/CubeSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+    public GameObject cubePrefab;
+    public ObjectPooler ObjectPooler;
+    public float spawnTimer;
+    public int size;
+
+    private void Start()
+    {
+        ObjectPooler = new ObjectPooler(cubePrefab, size);
+        spawnTimer = Time.time;
+    }
+
+    private void Update()
+    {
+        if(Time.time > spawnTimer + .1f)
+        {
+            spawnTimer = Time.time;
+
+            GameObject temp = ObjectPooler.Instance.GetObjectFromPool();
+            temp.SetActive(true);
+            temp.transform.position = new Vector3(Random.Range(-50, 50), Random.Range(-50, 50), Random.Range(-50, 50));
+
+            StartCoroutine(DestroyCube(temp));
+        }
+    }
+
+    private IEnumerator DestroyCube(GameObject _temp)
+    {
+        yield return new WaitForSeconds(Random.Range(1, 10));
+        ObjectPooler.Instance.ReturnObjectToPool(_temp);
+    }
+}
---
+++ Assets/ObjectPooler.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ObjectPooler
+{
+    public static ObjectPooler Instance;
+    private List<GameObject> objectPool;
+    private GameObject prefab;
+    private int size;
+
+    public ObjectPooler(GameObject _prefab, int _size)
+    {
+        if(Instance != null)
+        {
+            return;
+        }
+
+        Instance = this;
+        objectPool = new List<GameObject>();
+        prefab = _prefab;
+        size = _size;
+
+        GrowPool();
+    }
+
+    private void GrowPool()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            GameObject temp = GameObject.Instantiate(prefab);
+            temp.SetActive(false);
+            objectPool.Add(temp);
+        }
+    }
+
+    public GameObject GetObjectFromPool()
+    {
+        for (int i = 0; i < objectPool.Count; i++)
+        {
+            if(!objectPool[i].activeInHierarchy)
+            {
+                return objectPool[i];
+            }
+        }
+
+        GrowPool();
+
+        return GetObjectFromPool();
+    }
+
+    public void ReturnObjectToPool(GameObject _activeObject)
+    {
+        _activeObject.SetActive(false);
+    }
+}
